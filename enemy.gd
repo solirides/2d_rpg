01d6a0b8d94c70b6e70990291a1afe1a75acc220
solirides <83032760+solirides@users.noreply.github.com>
2025-11-14 00:09:37 -0800

    enemy attack

--- enemy.gd
+++ enemy.gd
@@ -8,11 +8,16 @@ var health = 100
 func _physics_process(delta):
 	if (player != null):
 		var dist = player.global_position - self.global_position
-		
 		linear_velocity += dist.normalized() * speed * delta
 	
 
 func damage(amount):
+	modulate = Color(1,0,0)
+	create_tween().tween_property(self, "modulate", Color(1,1,1), 0.4)
 	health -= amount
 	if health <= 0:
 		queue_free()
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		body.damage(20)
--- player.gd
+++ player.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
 
 @export var speed = 200
+@export var health = 100
 
 var dir = "right"
 var moving = false
 
 var projectile = preload("res://projectile.tscn")
+var enemy = preload("res://enemy.tscn")
 
 func _physics_process(delta):
 	
@@ -37,11 +39,25 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("fire"):
 		# create an instance of the projectile
 		var instance = projectile.instantiate()
+		# set position, rotation, and velocity based on cursor
 		var direction = get_global_mouse_position() - self.position
 		direction = direction.normalized()
 		instance.global_position = self.global_position
 		instance.rotation = atan2(direction.y, direction.x)
 		instance.linear_velocity = direction * 500
 		# add the instance to the scene
-		get_tree().get_current_scene().add_child(instance)
+		get_tree().get_root().add_child(instance)
 	
+	if Input.is_action_just_pressed("summon"):
+		var instance = enemy.instantiate()
+		instance.global_position = get_global_mouse_position()
+		instance.player = self
+		# add the instance to the scene
+		get_tree().get_root().add_child(instance)
+
+func damage(amount):
+	modulate = Color(1,0,0)
+	create_tween().tween_property(self, "modulate", Color(1,1,1), 0.4)
+	health -= amount
+	if health <= 0:
+		queue_free()
