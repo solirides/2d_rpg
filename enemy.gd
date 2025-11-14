4ddf83615245958d31b180664d6c68209493630b
solirides <83032760+solirides@users.noreply.github.com>
2025-11-13 21:12:43 -0800

    stuff

--- enemy.gd
+++ enemy.gd
@@ -2,11 +2,14 @@ extends RigidBody2D
 
 
 var health = 100
-var player
+@export var player:Node
+@export var speed = 200
 
 func _physics_process(delta):
 	if (player != null):
-		pass
+		var dist = player.global_position - self.global_position
+		
+		linear_velocity += dist.normalized() * speed * delta
 	
 
 func damage(amount):
--- projectile.gd
+++ projectile.gd
@@ -2,8 +2,11 @@ extends RigidBody2D
 
 func _on_body_entered(body: Node) -> void:
 	print("collide")
+	# if the body is an enemy, damage it and delete the projectile
 	if body.is_in_group("enemy"):
 		body.damage(20)
 		queue_free()
-	if (body.collision_layer & (100)) != 0: # hit wall
+	
+	# if the body is a wall (layer 3), delete the projectile
+	if body.get_collision_layer_value(3):
 		queue_free()
